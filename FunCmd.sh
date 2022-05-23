db581f707bc20648d0529703d99acd310d646bf4
pratik2630 <pratikkorgaonkar26@gmail.com>
2022-05-23 20:28:00 +0530

    .sh file added

---
+++ FunCmd.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/bash
+
+fortune | cowsay -f beavis.zen.cow 
+sleep 1
+fortune | cowsay -f blowfish.cow
+sleep 1
+fortune | cowsay -f bud-frogs.cow
+sleep 1
+fortune | cowsay -f bunny.cow
+sleep 1
+fortune | cowsay -f cheese.cow
+sleep 1
+fortune | cowsay -f cower.cow
+sleep 1
+fortune | cowsay -f default.cow
+sleep 1
+fortune | cowsay -f dragon-and-cow.cow
+sleep 1
+fortune | cowsay -f dragon.cow
+sleep 1
+fortune | cowsay -f elephant.cow
+sleep 1
+fortune | cowsay -f elephant-in-snake.cow
+sleep 1
+fortune | cowsay -f eyes.cow
+sleep 1
+fortune | cowsay -f flaming-sheep.cow
+sleep 1
+fortune | cowsay -f fox.cow
+sleep 1
+fortune | cowsay -f ghostbusters.cow
+sleep 1
+fortune | cowsay -f head-in.cow
+sleep 1
+fortune | cowsay -f hellokitty.cow
+sleep 1
+fortune | cowsay -f kiss.cow
+sleep 1
+fortune | cowsay -f kitty.cow 
+sleep 1
+fortune | cowsay -f koala.cow 
+sleep 1
+fortune | cowsay -f kosh.cow 
+sleep 1
+fortune | cowsay -f luke-koala.cow 
+sleep 1
+fortune | cowsay -f mech-and-cow.cow 
+sleep 1
+fortune | cowsay -f meow.cow 
+sleep 1
+fortune | cowsay -f milk.cow 
+sleep 1
+fortune | cowsay -f moofasa.cow 
+sleep 1
+fortune | cowsay -f moose.cow 
+sleep 1
+fortune | cowsay -f mutilated.cow 
+sleep 1
+fortune | cowsay -f ren.cow 
+sleep 1
+fortune | cowsay -f sheep.cow 
+sleep 1
+fortune | cowsay -f skeleton.cow 
+sleep 1
+fortune | cowsay -f small.cow 
+sleep 1
+fortune | cowsay -f stegosaurus.cow 
+sleep 1
+fortune | cowsay -f Stegosaurus.pm 
+sleep 1
+fortune | cowsay -f stimpy.cow 
+sleep 1
+fortune | cowsay -f supermilker.cow 
+sleep 1
+fortune | cowsay -f surgery.cow 
+sleep 1
+fortune | cowsay -f telebears.cow 
+sleep 1
+fortune | cowsay -f three-eyes.cow
+sleep 1
+fortune | cowsay -f turkey.cow
+sleep 1
+fortune | cowsay -f turtle.cow 
+sleep 1
+fortune | cowsay -f tux.cow  
+sleep 1
+fortune | cowsay -f udder.cow 
+sleep 1
+fortune | cowsay -f vader.cow 
+sleep 1
+fortune | cowsay -f vader-koala.cow 
+sleep 1
+fortune | cowsay -f www.cow 
+sleep 1
